c06f9d97712219746947d983fe6ad8d4b8c97679
Leigh Stewardson <leighlawhon@gmail.com>
2024-02-02 04:15:40 -0500

    adding twillio

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+from twilio.rest import Client
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+# Get the database URL from the environment variables
 
 
 app = Flask(__name__)
@@ -19,6 +26,18 @@ def process_query():
 
     return jsonify({"response": response_content})
 
+
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("AUTH_TOKEN")
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+  from_='whatsapp:+14155238886',
+  body='Your appointment is coming upnow',
+  to='whatsapp:+12028219726'
+)
+
+print(message.sid)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
